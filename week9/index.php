46915f998202beb30fab8aebe406f86c649a25eb
RicMem <119918376+RicMem@users.noreply.github.com>
2023-02-06 20:43:43 +0800

    Update index.php

--- week9/index.php
+++ week9/index.php
@@ -115,6 +115,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 
 	// Create connection
 	$conn = new mysqli($servername, $username, $password, $dbname);
+	
 	// Check connection
 	if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
