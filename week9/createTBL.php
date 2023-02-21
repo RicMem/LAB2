8c44628f40ada6433dbb53dc7014226105390b1d
RicMem <119918376+RicMem@users.noreply.github.com>
2023-02-21 23:10:48 +0800

    Update createTBL.php

--- week9/createTBL.php
+++ week9/createTBL.php
@@ -12,13 +12,7 @@ if ($conn->connect_error) {
 }
 
 // sql to create table
-$sql = "CREATE TABLE rbmemarion_myguests (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-firstname VARCHAR(30) NOT NULL,
-lastname VARCHAR(30) NOT NULL,
-email VARCHAR(50),
-reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-)";
+$sql = "CREATE TABLE rbmemarion_myguests (name VARCHAR(30) NOT NULL,email VARCHAR(50),website VARCHAR(50),comment VARCHAR(50),gender VARCHAR(50),reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP)";
 
 if ($conn->query($sql) === TRUE) {
   echo "Table MyGuests created successfully";
