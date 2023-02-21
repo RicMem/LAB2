2da28d00b4040be0109756543494b4be4ac15206
RicMem <119918376+RicMem@users.noreply.github.com>
2023-02-21 21:45:27 +0800

    Update index.php

--- week9/index.php
+++ week9/index.php
@@ -106,11 +106,11 @@ echo $gender;
 if ($_SERVER["REQUEST_METHOD"] == "POST") 
 {
 
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$dbname = "webprogss211
-	";
+	$servername = "192.168.150.213";
+        $username = "webprogss211";
+        $password = "fancyR!ce36";
+        $dbname = "webprogss211";
+
 
 	// Create connection
 	$conn = new mysqli($servername, $username, $password, $dbname);
