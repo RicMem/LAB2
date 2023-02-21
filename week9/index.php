8849bea8d1dedefa0e6f9725b5219c3a9f30f76f
RicMem <119918376+RicMem@users.noreply.github.com>
2023-02-21 22:30:50 +0800

    Update index.php

--- week9/index.php
+++ week9/index.php
@@ -119,7 +119,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 	die("Connection failed: " . $conn->connect_error);
 	}
 
-	$sql = "INSERT INTO canicanor_myguests (name, email, website, comment, gender)
+	$sql = "INSERT INTO rbmemarion_myguests (name, email, website, comment, gender)
   VALUES ('$name','$email','$website','$comment','$gender')";
 	if ($conn->query($sql) === TRUE) {
 	echo "New record created successfully";
